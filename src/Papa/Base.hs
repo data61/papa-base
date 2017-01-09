86010c417886d9838a2034cf85bd6123163a8949
Tony Morris <tmorris@tmorris.net>
2017-01-09 13:07:58 +1000

    change log

--- src/Papa/Base.hs
+++ src/Papa/Base.hs
@@ -4,11 +4,5 @@ module Papa.Base(
   module P
 ) where
 
-import Papa.Base.Control.Applicative as P
-import Papa.Base.Control.Category as P
-import Papa.Base.Data.Foldable as P
-import Papa.Base.Data.Functor as P
-import Papa.Base.Data.List as P
-import Papa.Base.Data.Monoid as P
-import Papa.Base.Data.Traversable as P
-import Papa.Base.Prelude as P
+import Papa.Base.Export as P
+import Papa.Base.Implement as P
